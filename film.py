174f9a87708fe98d219cda9268d77818facbb394
Abigail Fox <abigailfireflyfox@gmail.com>
2020-11-22 22:17:12 -0500

    starting justwatch parsing

--- film.py
+++ film.py
@@ -1,5 +1,7 @@
 import requests
 import csv
+import justwatch
+from justwatch import JustWatch
 from bs4 import BeautifulSoup
 
 response = requests.get(
@@ -21,6 +23,11 @@ for year in years:
     for movie in movies:
         #print(movie)
         link = movie.find("a")
+        #title = movie.text.strip()
+        #if title.find('-') != -1:
+            #print(title[title.find('-'):])
+            #title = title[:title.find('-')]
+            #print(title)
         stripped_movies.append(movie.text.strip())
         if link != None:
             link = "https://en.wikipedia.org" + link['href']
@@ -33,3 +40,11 @@ for year in years:
     with open(csv_name, 'w', newline='') as csvfile:
         movie_writer = csv.writer(csvfile, delimiter=',')
         movie_writer.writerow(stripped_movies + stripped_links)
+        
+#find out where streaming
+#manually set header to avoid api error
+justwatch.justwatchapi.HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+just_watch = JustWatch(country='US')
+#test query
+results = just_watch.search_for_item(query='the matrix')
+print(results['items'][0]['offers'])
