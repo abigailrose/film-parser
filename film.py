2eaef5ba01ac2a25b6a9330032e543a955074162
Abigail Fox <abigailfireflyfox@gmail.com>
2020-11-22 21:25:54 -0500

    finding which have links

--- film.py
+++ film.py
@@ -14,18 +14,10 @@ for year in years:
     full_list = soup.find(id=year).findNext(class_="multicol")
     links = full_list.find_all("a")
     movies = full_list.find_all("li")
-    print(full_list)
+    print(movies)
+    #print(full_list)
     for movie in movies:
-        print(movie)
-        #print(movie.text.strip())
-yr2 = soup.find(id="2006")
-#print(yr2)
-
-curr = yr2
-#print()
-
-#while "multicol" not in curr:
-#    print(curr.next_element)
-#    curr = curr.next_element
-#print(curr)
-#allYears[0].find_all("a")
+        #print(movie)
+        link = movie.find("a")
+        print(link)
+        #print(movie.text.strip()
