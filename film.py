869900358a883677757b49d23bc3a2da6aee9380
Abigail Fox <abigailfireflyfox@gmail.com>
2020-11-22 22:25:03 -0500

    parsed out award names

--- film.py
+++ film.py
@@ -13,27 +13,35 @@ soup = BeautifulSoup(response.content, 'html.parser')
 years = list(range(2005, 2019))
 print(years)
 
+#find out where streaming
+#manually set header to avoid api error
+justwatch.justwatchapi.HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+just_watch = JustWatch(country='US')
+
 for year in years:
     full_list = soup.find(id=year).findNext(class_="multicol")
     links = full_list.find_all("a")
     movies = full_list.find_all("li")
     stripped_links = []
     stripped_movies = []
+    where_watch = []
     #print(full_list)
     for movie in movies:
         #print(movie)
+        watch = []
         link = movie.find("a")
-        #title = movie.text.strip()
-        #if title.find('-') != -1:
+        title = movie.text.strip()
+        if title.find(' - ') != -1:
             #print(title[title.find('-'):])
-            #title = title[:title.find('-')]
-            #print(title)
+            title = title[:title.find('-')]
+            print(title)
         stripped_movies.append(movie.text.strip())
         if link != None:
             link = "https://en.wikipedia.org" + link['href']
         else:
             link = ""
         stripped_links.append(link)
+        #results = just_watch.search_for_item(query='the matrix')
     #print(stripped_movies)
     #print(stripped_links)
     csv_name = str(year) + ".csv"
@@ -41,10 +49,13 @@ for year in years:
         movie_writer = csv.writer(csvfile, delimiter=',')
         movie_writer.writerow(stripped_movies + stripped_links)
         
-#find out where streaming
-#manually set header to avoid api error
-justwatch.justwatchapi.HEADER = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-just_watch = JustWatch(country='US')
 #test query
-results = just_watch.search_for_item(query='the matrix')
-print(results['items'][0]['offers'])
+#results = just_watch.search_for_item(query='the matrix')
+#print(results['items'][0]['offers'])
+#offers = results['items'][0]['offers']
+
+#for offer in offers:
+#    print(offer['monetization_type'])
+#    if offer['monetization_type'] == 'flatrate':
+#        print(offer['urls']['standard_web'])
+
